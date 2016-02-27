2e772dcc5240d88cdc72b72ef85f6807e88eb65e
Paulo Santos <paulo_sss@hotmail.com>
2016-02-27 20:06:00 +0000

    4. Forms and events

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,27 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function(event){
+      // Prevent default behavior of browser
+      event.preventDefault();
+
+      // Get text from the form
+      var text = event.target.text.value;
+
+      // Insert text into the database
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      // Clears the form
+      event.target.text.value = "";
+
     }
   });
 }
