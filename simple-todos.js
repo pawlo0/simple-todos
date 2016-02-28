7c56af695f98db20fb940c7b31851eb53d269fa1
Paulo Santos <paulo_sss@hotmail.com>
2016-02-28 00:01:10 +0000

    8.Temporary UI state

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,17 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")){
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function(){
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();      
     }
   });
 
@@ -24,7 +34,9 @@ if (Meteor.isClient) {
 
       // Clears the form
       event.target.text.value = "";
-
+    },
+    "change .hide-completed input": function(){
+      Session.set("hide-completed", event.target.checked);
     }
   });
 
