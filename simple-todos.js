04aacabd0e020785f1847943a3fc14ec5058ebac
Paulo Santos <paulo_sss@hotmail.com>
2016-02-28 17:27:44 +0000

    10. Security with methods

--- simple-todos.js
+++ simple-todos.js
@@ -27,12 +27,7 @@ if (Meteor.isClient) {
       var text = event.target.text.value;
 
       // Insert text into the database
-      Tasks.insert({
-        text: text,
-        createdAt: new Date(),
-        owner: Meteor.userId(),
-        username: Meteor.user().username
-      });
+      Meteor.call("addTask", text);
 
       // Clears the form
       event.target.text.value = "";
@@ -45,12 +40,10 @@ if (Meteor.isClient) {
   Template.task.events({
     "click .toggle-checked": function(){
       // It sets the checked property to the opposite of its current value
-       Tasks.update(this._id, {
-         $set: {checked: ! this.checked}
-       });
+      Meteor.call("setChecked", this._id, ! this.checked);
     },
     "click .delete": function(){
-      Tasks.remove(this._id);
+      Meteor.call("deleteTask", this._id);
     }
   });
 
@@ -58,3 +51,24 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 }
+
+Meteor.methods({
+  addTask: function(text){
+    if(! Meteor.userId()){
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function(taskId){
+    Tasks.remove(taskId);
+  },
+  setChecked: function(taskId, setChecked){
+    Tasks.update(taskId, {$set: {checked: setChecked}});
+  }
+});
